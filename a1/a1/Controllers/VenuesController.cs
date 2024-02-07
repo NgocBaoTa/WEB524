1a4ee8ee96ec94e00524374bef3bf1ab5b1841b1
Bao Ngoc Ta <baongoc.ta04@gmail.com>
2024-02-06 23:21:29 -0500

    VenuesController - 'get all' use case

---
+++ a1/a1/Controllers/VenuesController.cs
@@ -0,0 +1,20 @@
+﻿using a1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace a1.Controllers
+{
+    public class VenuesController : Controller
+    {
+        private Manager m = new Manager();
+
+        // GET: Venues
+        public ActionResult Index()
+        {
+            return View(m.VenueGetAll());
+        }
+    }
+}
