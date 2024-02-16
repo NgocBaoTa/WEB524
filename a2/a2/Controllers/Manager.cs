1c9b8c0ccac95f2ca34ca0aa921766ea5ce1cfb9
Bao Ngoc Ta <baongoc.ta04@gmail.com>
2024-02-15 23:46:57 -0500

    assingment 2

---
+++ a2/a2/Controllers/Manager.cs
@@ -0,0 +1,62 @@
+﻿using a2.Data;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// ************************************************************************************
+// WEB524 Project Template V1 == 2231-c60b75ee-c962-421e-a18a-8ce1cdcbc4b9
+//
+// By submitting this assignment you agree to the following statement.
+// I declare that this assignment is my own work in accordance with the Seneca Academic
+// Policy. No part of this assignment has been copied manually or electronically from
+// any other source (including web sites) or distributed to other students.
+// ************************************************************************************
+
+namespace a2.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private DataContext ds = new DataContext();
+
+        // AutoMapper instance
+        public IMapper mapper;
+
+        public Manager()
+        {
+            // If necessary, add more constructor code here...
+
+            // Configure the AutoMapper components
+            var config = new MapperConfiguration(cfg =>
+            {
+                // Define the mappings below, for example...
+                // cfg.CreateMap<SourceType, DestinationType>();
+                // cfg.CreateMap<Product, ProductBaseViewModel>();
+
+            });
+
+            mapper = config.CreateMapper();
+
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+        }
+
+
+        // Add your methods below and call them from controllers. Ensure that your methods accept
+        // and deliver ONLY view model objects and collections. When working with collections, the
+        // return type is almost always IEnumerable<T>.
+        //
+        // Remember to use the suggested naming convention, for example:
+        // ProductGetAll(), ProductGetById(), ProductAdd(), ProductEdit(), and ProductDelete().
+
+
+
+    }
+}
---
+++ a2/a2/Data/InvoiceLine.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace a2.Data
+{
+    [Table("InvoiceLine")]
+    public class InvoiceLine
+    {
+
+        #region Columns
+
+        public int InvoiceLineId { get; set; }
+
+        public int InvoiceId { get; set; }
+
+        public int TrackId { get; set; }
+
+        [Column(TypeName = "numeric")]
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        #endregion
+
+        #region Navigation Properties
+
+        public Invoice Invoice { get; set; }
+
+        public Track Track { get; set; }
+
+        #endregion
+
+    }
+}
